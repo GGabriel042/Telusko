6da830770c0c58b83a9abe8c7c78b0c58d9660b2
GGabriel042 <gabrielgonzaga042@gmail.com>
2025-04-15 18:08:50 -0300

    Switch

--- src/Hello.java
+++ src/Hello.java
@@ -1,11 +1,29 @@
 public class Hello {
     public static void main(String[] args) {
-        int a = 8;
+        int n = 5;
 
-        if (a > 10) {
-            System.out.println("Hello");
-        } else {
-            System.out.println("Bye");
+        switch (n) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
         }
     }
 }
