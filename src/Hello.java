5290b7be4a32ee0c1eb3bf90c1e4be8eec947de6
GGabriel042 <gabrielgonzaga042@gmail.com>
2025-04-15 16:37:55 -0300

    Numbers variables

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,11 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        float num1 = 30;
+        float num2 = 7;
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 / num2);
+        System.out.println(num1 * num2);
+        System.out.println(num1 % num2);
     }
 }
