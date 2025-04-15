17f1096271cbe5a20fdad4996c83b2379dfbfa4c
GGabriel042 <gabrielgonzaga042@gmail.com>
2025-04-15 18:24:03 -0300

    While nested loop

--- src/Hello.java
+++ src/Hello.java
@@ -2,9 +2,16 @@ public class Hello {
     public static void main(String[] args) {
         int i = 1;
 
-        while (i < 10) {
+        while (i < 5) {
             System.out.println("Hi "+ i);
             i++;
+
+            int j = 1;
+            while (j < 5) {
+                System.out.println("Hello "+j);
+                j++;
+            }
+            System.out.println("---------------");
         }
     }
 }
