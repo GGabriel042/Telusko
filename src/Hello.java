f381720ef9f88ac787a221e079c5838b26cf4064
GGabriel042 <gabrielgonzaga042@gmail.com>
2025-04-15 17:43:00 -0300

    booleans 2

--- src/Hello.java
+++ src/Hello.java
@@ -1,12 +1,13 @@
 public class Hello {
     public static void main(String[] args) {
-        boolean verdadeiro = true;
-        boolean falso = false;
+        int a = 7;
+        int b = 5;
+        int c = 5;
+        int d = 9;
 
-        if (verdadeiro && falso) {
-            System.out.println("verdadeiro");
-        } else {
-            System.out.println("falso");
-        }
+        boolean result = a > b && c < d;
+        boolean result2 = a > b && c > d;
+        System.out.println(result);
+        System.out.println(result2);
     }
 }
