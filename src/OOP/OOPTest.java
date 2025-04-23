0a2ea2f041c42357fcef66d0424efa6aac18622e
GGabriel042 <gabrielgonzaga042@gmail.com>
2025-04-23 16:59:10 -0300

    OOP commit

---
+++ src/OOP/OOPTest.java
@@ -0,0 +1,16 @@
+package OOP;
+
+class Calculator {
+    public int add(int num1, int num2) {
+        return num1 + num2;
+    }
+}
+
+public class OOPTest {
+    public static void main(String[] args) {
+
+        Calculator calc = new Calculator();
+        System.out.println(calc.add(5, 4));
+
+    }
+}
