1b3dcfe23202294cd18ecab18b7407f10ed409a7
GGabriel042 <gabrielgonzaga042@gmail.com>
2025-04-24 17:39:10 -0300

    Encapsulation part 2

--- src/E_Modificadores/Encapsulation.java
+++ src/E_Modificadores/Encapsulation.java
@@ -4,20 +4,20 @@ class Human {
     private int age;
     private String name;
 
-    public int getAge(){
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getName(){
+    public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
 
