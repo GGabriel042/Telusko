802971ad9b9149077b16a800f034db41b7075b34
GGabriel042 <gabrielgonzaga042@gmail.com>
2025-04-23 22:49:12 -0300

    Static variables

---
+++ src/E_Modificadores/TipoDeRetornoTest02.java
@@ -0,0 +1,36 @@
+package E_Modificadores;
+
+class Mobile02 {
+    String brand;
+    int price;
+    static String name;
+
+    static {
+        name = "Phone";
+        System.out.println("In static block");
+    }
+
+    public Mobile02() {
+        brand = "";
+        price = 200;
+        System.out.println("In constructor");
+    }
+
+    public void show() {
+        System.out.println(brand + " " + price + " " + name);
+    }
+}
+
+public class TipoDeRetornoTest02 {
+    public static void main(String[] args) throws ClassNotFoundException {
+        Mobile02 obj1 = new Mobile02();
+        obj1.brand = "Apple";
+        obj1.price = 1500;
+        Mobile02.name = "SmartPhone";
+
+        Mobile02 obj2 = new Mobile02();
+
+
+//        Class.forName("Mobile02");
+    }
+}
