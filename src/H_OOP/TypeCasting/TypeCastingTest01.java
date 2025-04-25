420c5780f49a7a1a0463ae9dcecbe536bc4e0f2c
GGabriel042 <gabrielgonzaga042@gmail.com>
2025-04-25 18:35:07 -0300

    Type Casting part 2

--- src/H_OOP/TypeCasting/TypeCastingTest01.java
+++ src/H_OOP/TypeCasting/TypeCastingTest01.java
@@ -13,13 +13,15 @@ class B extends A {
 
 public class TypeCastingTest01 {
     public static void main(String[] args) {
-        double d = 4.5;
-        int i = (int) d;
+//        double d = 4.5;
+//        int i = (int) d;
+//
+//        System.out.println(i);
 
-        System.out.println(i);
-
-        A obj = new A();
+        A obj = (A) new B();
         obj.show1();
 
+        B obj2 = (B) obj;
+        obj2.show2();
     }
 }
