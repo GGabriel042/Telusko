de76aacfa5b2fa6c1360399f30b040c4d03a951e
GGabriel042 <gabrielgonzaga042@gmail.com>
2025-04-24 21:50:12 -0300

    Modificadores public, default, protected e private

---
+++ src/E_Modificadores/Modificators.java
@@ -0,0 +1,11 @@
+package E_Modificadores;
+
+public class Modificators {
+
+//    public           pode ser acessado de qualquer lugar
+//    defaut           pode ser acessado de dentro do mesmo package
+//    protected        pode ser acessado de dentro de um package diferente se a classe for extendida
+//    private          pode ser acessado de dentro da mesma classe
+
+
+}
