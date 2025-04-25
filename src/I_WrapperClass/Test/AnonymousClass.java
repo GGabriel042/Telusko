fb00d8a0d3910a19ef8662392cc65fdb2450231e
GGabriel042 <gabrielgonzaga042@gmail.com>
2025-04-25 20:29:38 -0300

    Anonymous Inner Class abstract

--- src/I_WrapperClass/Test/AnonymousClass.java
+++ src/I_WrapperClass/Test/AnonymousClass.java
@@ -1,21 +1,24 @@
 package I_WrapperClass.Test;
 
-class X {
-    public void show() {
-        System.out.println("In X Show");
-    }
+abstract class X {
+    abstract public void show();
+    abstract public void config();
 }
-
-
 
 public class AnonymousClass {
     public static void main(String[] args) {
+
         X obj = new X() {
             public void show() {
-                System.out.println("In new show");
+                System.out.println("In new Show");
+            }
+
+            public void config() {
+                System.out.println("In new config");
             }
         };
 
         obj.show();
+        obj.config();
     }
 }
