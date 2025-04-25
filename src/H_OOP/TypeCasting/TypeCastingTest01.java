e71539095e9cae388879173cb3e53f131073d809
GGabriel042 <gabrielgonzaga042@gmail.com>
2025-04-25 18:26:56 -0300

    Type Casting part 1

---
+++ src/H_OOP/TypeCasting/TypeCastingTest01.java
@@ -0,0 +1,25 @@
+package H_OOP.TypeCasting;
+
+class A {
+    public void show1(){
+        System.out.println("In A show");
+    }
+}
+class B extends A {
+    public void show2(){
+        System.out.println("In B show");
+    }
+}
+
+public class TypeCastingTest01 {
+    public static void main(String[] args) {
+        double d = 4.5;
+        int i = (int) d;
+
+        System.out.println(i);
+
+        A obj = new A();
+        obj.show1();
+
+    }
+}
