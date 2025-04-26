01ce0a8d429b18526b25f70b54b7d1db5b84af5d
GGabriel042 <gabrielgonzaga042@gmail.com>
2025-04-25 23:20:42 -0300

    Enum part 2

--- src/K_Enum/EnumTest01.java
+++ src/K_Enum/EnumTest01.java
@@ -6,16 +6,35 @@ enum Status {
 
 public class EnumTest01 {
     public static void main(String[] args) {
-        Status sr = Status.Running;
-        System.out.println(sr);
-        System.out.println(sr.ordinal());
+//        Status sr = Status.Running;
+//        System.out.println(sr);
+//        System.out.println(sr.ordinal());
+//
+//        Status[] sAll = Status.values();
+//
+//        System.out.println("----------");
+//        for (Status s : sAll) {
+//            System.out.println(s + " : " + s.ordinal());
+//        }
 
-        Status[] sAll = Status.values();
+        Status s = Status.Failed;
 
-        System.out.println("----------");
-        for (Status s : sAll) {
-            System.out.println(s + " : " + s.ordinal());
+
+        switch (s){
+            case Running -> System.out.println("All good");
+            case Failed -> System.out.println("Try again");
+            case Pending -> System.out.println("Please wait");
+            default -> System.out.println("Done");
         }
+
+
+        if (s == Status.Running) {
+            System.out.println("All good");
+        } else if (s == Status.Failed) {
+            System.out.println("Try again");
+        } else if (s == Status.Pending) {
+            System.out.println("Please wait");
+        } else System.out.println("Done");
 
     }
 }
