e5ba50e2a391f43e77ef41152bb60b350f377090
GGabriel042 <134812062+GGabriel042@users.noreply.github.com>
2025-05-27 16:32:36 -0300

    C_Objects

---
+++ src/C_Objects/StudentObjectTest.java
@@ -0,0 +1,40 @@
+package C_Objects;
+
+class Student {
+    int rollno;
+    String name;
+    int marks;
+}
+
+public class StudentObjectTest {
+    public static void main(String[] args) {
+
+        Student s1 = new Student();
+        s1.rollno = 1;
+        s1.name = "Gabriel";
+        s1.marks = 88;
+
+        Student s2 = new Student();
+        s2.rollno = 2;
+        s2.name = "Gonzaga";
+        s2.marks = 67;
+
+        Student s3 = new Student();
+        s3.rollno = 3;
+        s3.name = "Navin";
+        s3.marks = 97;
+
+        Student students[] = new Student[3];
+        students[0] = s1;
+        students[1] = s2;
+        students[2] = s3;
+
+//        for (int i = 0; i < students.length; i++) {
+//            System.out.println(students[i].name + " : " + students[i].marks);
+//        }
+
+        for (Student student : students) {
+            System.out.println(student.name + " : " + student.marks);
+        }
+    }
+}
