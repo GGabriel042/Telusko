bff40de0d1256b14755a6d6efdf04ec0efaaaaab
GGabriel042 <gabrielgonzaga042@gmail.com>
2025-07-02 10:27:39 -0300

    Types of interface part 2

--- src/M_TypesOfInterface/TypesOfInterfaceTest01.java
+++ src/M_TypesOfInterface/TypesOfInterfaceTest01.java
@@ -2,18 +2,13 @@ package M_TypesOfInterface;
 
 @FunctionalInterface
 interface A {
-    void show();
+    void show(int i);
 //    void run();
 }
 
-
 public class TypesOfInterfaceTest01 {
     public static void main(String[] args) {
-        A obj = new A() {
-            public void show() {
-                System.out.println("in show");
-            }
-        };
-        obj.show();
+        A obj = i -> System.out.println("in show " + i);
+        obj.show(5);
     }
 }
