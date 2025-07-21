edfb76f82d9cb1b8098db92683e1df34da058cdc
GGabriel042 <gabrielgonzaga042@gmail.com>
2025-07-21 15:20:35 -0300

    Error / Try and Catch

---
+++ src/N_Error/ErrorTest01.java
@@ -0,0 +1,4 @@
+package N_Error;
+
+public class ErrorTest01 {
+}
---
+++ src/N_Error/ErrorTest02.java
@@ -0,0 +1,21 @@
+package N_Error;
+
+public class ErrorTest02 {
+    public static void main(String[] args) {
+
+        int i = 0;
+        int j = 0;
+
+        try
+        {
+            j = 8/i;
+        } catch (Exception e) {
+            System.out.println("Something went wrong");
+        }
+
+        System.out.println(j);
+
+
+        System.out.println("Bye");
+    }
+}
