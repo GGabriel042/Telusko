91c9511227ca266178a13b997e9b540acb4176b8
GGabriel042 <134812062+GGabriel042@users.noreply.github.com>
2025-08-26 18:56:24 -0300

    L_Annotations

---
+++ src/L_Annotations/AnnotationsTest01.java
@@ -0,0 +1,21 @@
+package L_Annotations;
+
+class A {
+    public void showTheClass() {
+        System.out.println("in show A");
+    }
+}
+
+class B extends A {
+    @Override
+    public void showTheClass() {
+        System.out.println("in show B");
+    }
+}
+
+public class AnnotationsTest01 {
+    public static void main(String[] args) {
+        B obj = new B();
+        obj.showTheClass();
+    }
+}
