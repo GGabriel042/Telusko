ee64c9a432eec1498b46a91baf4df46286d024c8
GGabriel042 <134812062+GGabriel042@users.noreply.github.com>
2025-08-30 16:08:09 -0300

    F_NamingConvention

---
+++ src/F_NamingConvention/NamingConvention.java
@@ -0,0 +1,16 @@
+package F_NamingConvention;
+
+public class NamingConvention {
+    public static void main(String[] args) {
+
+    }
+}
+
+
+
+// class and interface - Calc, Runnable
+// variable and method - marks, show()
+// constants - PIE, BRAND
+
+// showMyMarks()
+// show_my_marks()          it work but not recommended
